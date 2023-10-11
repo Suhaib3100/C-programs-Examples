1a0b3ddd1e1ded7584eff5237289fd247631a311
Suhaib <109773912+Suhaib3100@users.noreply.github.com>
2023-10-11 21:48:50 +0530

    Create Program7.c

---
+++ Program7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    
+            int x;
+                   scanf("%d",&x);
+                    
+                switch(x)
+                {
+                    
+                
+                case 1:printf("case 1 is executed:");
+                    break;
+                case 2:printf("Case q is executed:");
+                    break;
+                case 3:printf("Case z is executed:");
+                    break;
+                default:printf("Mismatch Output");
+                
+                }
+        return 0;
+                
+            
+}
